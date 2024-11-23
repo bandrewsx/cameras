6710b6271e401ac1d0467c2ad414b37c5915f676
Brianna Andrews <bwoandrews@gmail.com>
2024-11-23 11:17:42 -0700

    Update cameras.js

--- cameras.js
+++ cameras.js
@@ -1,4 +1,5 @@
 const cameras = [
+  // Original 7 camera streams
   {
     "id": "camera-1",
     "name": "28th St and Colorado Ave",
@@ -55,348 +56,150 @@ const cameras = [
     "thumbnail": "",
     "stream": "https://publicstreamer3.cotrip.org/rtplive/036W05465CAM1MED/chunklist_w671228802.m3u8"
   },
+
+  // Newly added camera streams
   {
-    "id": "camera-8",
-    "name": "I-70 MP 278.8 at Denver",
-    "latitude": 39.7392,
-    "longitude": -104.9903,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70/stream.m3u8"
+    id: "camera-8",
+    name: "US-36 @ CO-157 (W View)",
+    latitude: 39.985155,
+    longitude: -105.232497,
+    thumbnail: "https://ie.trafficland.com/v2.0/10744/full?system=weatherbug-web&pubtoken=07a3ca255962aba7f182ad0ccd6c8f7777830b3b661570181bbb42ec9ac7f454&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.985155,-105.232497&camId=10744"
   },
   {
-    "id": "camera-9",
-    "name": "Federal Blvd and 84th Ave",
-    "latitude": 39.8683,
-    "longitude": -105.0245,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/federal/stream.m3u8"
+    id: "camera-9",
+    name: "CO 93 MP 013.65 NB at Eldorado Springs Dr/CO 170",
+    latitude: 39.953704,
+    longitude: -105.231680,
+    thumbnail: "https://ie.trafficland.com/v2.0/435344/full?system=weatherbug-web&pubtoken=0a1e9e5a64682102d6cde32fe72b5b507a42d9db7168710eee0659a9c439fd52&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.953704,-105.231680&camId=435344"
   },
   {
-    "id": "camera-10",
-    "name": "I-25 and Dry Creek",
-    "latitude": 39.5776,
-    "longitude": -104.9065,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-drycreek/stream.m3u8"
+    id: "camera-10",
+    name: "CO 93 MP 010.40 SB: 1.7 miles S of CO 128 in Golden",
+    latitude: 39.909969,
+    longitude: -105.240801,
+    thumbnail: "https://ie.trafficland.com/v2.0/435301/full?system=weatherbug-web&pubtoken=384b9118cd86f389ce8df6d0566de6b58771eef2c770b8b316818315e0a21c55&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.909969,-105.240801&camId=435301"
   },
   {
-    "id": "camera-11",
-    "name": "Denver Int'l Airport Entrance",
-    "latitude": 39.8617,
-    "longitude": -104.6735,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/airport/stream.m3u8"
+    id: "camera-11",
+    name: "US-36 w/o 88th St (E View)",
+    latitude: 39.94688,
+    longitude: -105.149291,
+    thumbnail: "https://ie.trafficland.com/v2.0/421040/full?system=weatherbug-web&pubtoken=87282cbed8fbc5803ab63f0e0ba7892981429d8de0097d82362181c7a5a8de9b&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.94688,-105.149291&camId=421040"
   },
   {
-    "id": "camera-12",
-    "name": "I-70 MP 209.2 at Beaver Brook",
-    "latitude": 39.7456,
-    "longitude": -105.5632,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-beaverbrook/stream.m3u8"
+    id: "camera-12",
+    name: "CO 93 MP 008.80 SB at Westgate Rd in Golden",
+    latitude: 39.885171,
+    longitude: -105.241175,
+    thumbnail: "https://ie.trafficland.com/v2.0/435327/full?system=weatherbug-web&pubtoken=33255a7c5c60f1e49fe14927eeb38325e8418609b402bc197ecf3cc39beb30bf&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.885171,-105.241175&camId=435327"
   },
   {
-    "id": "camera-13",
-    "name": "US 40 at Berthoud Falls",
-    "latitude": 40.2974,
-    "longitude": -105.7415,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us40-berthoud/stream.m3u8"
+    id: "camera-13",
+    name: "CO-128 MP 007.20 WB at Interlocken Pkwy",
+    latitude: 39.916798,
+    longitude: -105.11132,
+    thumbnail: "https://ie.trafficland.com/v2.0/435401/full?system=weatherbug-web&pubtoken=e9867435437f0917b2dcc6bc9f2e4dbb2a25a871cd23ea698b739409ab32db10&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.916798,-105.11132&camId=435401"
   },
   {
-    "id": "camera-14",
-    "name": "CO 93 and US 6",
-    "latitude": 39.7439,
-    "longitude": -105.2268,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/co93-us6/stream.m3u8"
+    id: "camera-14",
+    name: "CO 93 MP 006.85 NB: 0.8 miles S of Coal Creek Canyon Rd/CO 72",
+    latitude: 39.858465,
+    longitude: -105.234066,
+    thumbnail: "https://ie.trafficland.com/v2.0/435389/full?system=weatherbug-web&pubtoken=fbb241d5c905816c0cc3934fef487e4fee89da497031a026679eec5630c807d3&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.858465,-105.234066&camId=435389"
   },
   {
-    "id": "camera-15",
-    "name": "I-25 and Castle Rock",
-    "latitude": 39.3720,
-    "longitude": -104.8509,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-castle/stream.m3u8"
+    id: "camera-15",
+    name: "CO 93 MP 010.40 SB: 1.7 miles S of CO 128 in Golden",
+    latitude: 39.909969,
+    longitude: -105.240801,
+    thumbnail: "https://ie.trafficland.com/v2.0/435301/full?system=weatherbug-web&pubtoken=384b9118cd86f389ce8df6d0566de6b58771eef2c770b8b316818315e0a21c55&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.909969,-105.240801&camId=435301"
   },
   {
-    "id": "camera-16",
-    "name": "I-70 MP 245.2 at Genesee",
-    "latitude": 39.6531,
-    "longitude": -105.2912,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-genesee/stream.m3u8"
+    id: "camera-16",
+    name: "I-25 MP 229.10 SB at E Baseline Rd/CO 7",
+    latitude: 40.000301,
+    longitude: -104.981468,
+    thumbnail: "https://ie.trafficland.com/v2.0/435333/full?system=weatherbug-web&pubtoken=d730f689882732fccc2a5bf3bd039f266e73d91a7fc9893c87a80fc9329ac392&refreshRate=600000",
+    stream: "traffic-cam/?latlng=40.000301,-104.981468&camId=435333"
   },
   {
-    "id": "camera-17",
-    "name": "I-25 at US 34",
-    "latitude": 40.3764,
-    "longitude": -104.7224,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-us34/stream.m3u8"
+    id: "camera-17",
+    name: "CO 93 MP 002.50 NB : 0.6 miles S of W 56th Ave in Golden",
+    latitude: 39.791873,
+    longitude: -105.230278,
+    thumbnail: "https://ie.trafficland.com/v2.0/435334/full?system=weatherbug-web&pubtoken=049aa1d6d5d718c170236ea780e833b3b41ab688bd4ce5ee8bc70f18eb54eeca&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.791873,-105.230278&camId=435334"
   },
   {
-    "id": "camera-18",
-    "name": "I-70 MP 226.5 at Morrison",
-    "latitude": 39.6538,
-    "longitude": -105.2437,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-morrison/stream.m3u8"
+    id: "camera-18",
+    name: "I-70 MP 265.70 EB at Ward Rd",
+    latitude: 39.77964288,
+    longitude: -105.1382644,
+    thumbnail: "https://ie.trafficland.com/v2.0/435232/full?system=weatherbug-web&pubtoken=3eb31168e15c23b10a6d8dd99f362dbc0c0fd2054244c0a27c9451697db5434&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.77964288,-105.1382644&camId=435232"
   },
   {
-    "id": "camera-19",
-    "name": "US 285 at Littleton Blvd",
-    "latitude": 39.6054,
-    "longitude": -105.0492,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us285-littleton/stream.m3u8"
+    id: "camera-19",
+    name: "US-6 MP 271.60 WB at CO 58/CO 93 in Golden",
+    latitude: 39.867686,
+    longitude: -104.986632,
+    thumbnail: "https://ie.trafficland.com/v2.0/435264/full?system=weatherbug-web&pubtoken=83a404d636a6d5194e64903a543961add8025a05516e903069a95cf4ae1adb4f&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.867686,-104.986632&camId=10516"
   },
   {
-    "id": "camera-20",
-    "name": "I-70 MP 202.5 at Georgetown",
-    "latitude": 39.7103,
-    "longitude": -105.7805,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-georgetown/stream.m3u8"
+    id: "camera-20",
+    name: "I-70 MP 244.35 EB at US 6/US 40",
+    latitude: 39.740001,
+    longitude: -105.227571,
+    thumbnail: "https://ie.trafficland.com/v2.0/435410/full?system=weatherbug-web&pubtoken=054967bf548aef059513d419622f13240ddaca85fc02e23fe99c123aa38ca361&refreshRate=600000",
+    stream: "traffic-cam/?latlng=39.740001,-105.227571&camId=435410"
   },
   {
-    "id": "camera-21",
-    "name": "I-25 at Santa Fe Dr",
-    "latitude": 39.6736,
-    "longitude": -104.9744,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-santa-fe/stream.m3u8"
+      id: "camera-21",
+      name: "US-36 e/o Lowell Blvd (E View)",
+      latitude: 39.838279,
+      longitude: -105.031287,
+      thumbnail: "https://ie.trafficland.com/v2.0/6823/full?system=weatherbug-web&pubtoken=f84f4ac7370741e6c60806bc8917cd69a56974de227a785e56eb95ceff5492e7&refreshRate=600000",
+      stream: "traffic-cam/?latlng=39.838279,-105.031287&camId=6823"
   },
   {
-    "id": "camera-22",
-    "name": "I-76 MP 0.1 at Brighton Blvd",
-    "latitude": 39.7530,
-    "longitude": -104.9901,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i76-brighton/stream.m3u8"
+      id: "camera-22",
+      name: "I-25 s/o Thornton Pkwy (N View)",
+      latitude: 39.862144,
+      longitude: -104.987889,
+      thumbnail: "https://ie.trafficland.com/v2.0/10514/full?system=weatherbug-web&pubtoken=2d40024e4a7875d3a6825e1a79820b08f1a76dca23ab3681b58406dd334382af&refreshRate=600000",
+      stream: "traffic-cam/?latlng=39.862144,-104.987889&camId=10514"
   },
   {
-    "id": "camera-23",
-    "name": "I-270 MP 3.6 at Vasquez Blvd",
-    "latitude": 39.7868,
-    "longitude": -104.8771,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i270-vasquez/stream.m3u8"
+      id: "camera-23",
+      name: "I-70 MP 265.70 EB at Ward Rd",
+      latitude: 39.790728,
+      longitude: -104.993914,
+      thumbnail: "https://ie.trafficland.com/v2.0/435226/full?system=weatherbug-web&pubtoken=40a0a7ebfec1abfd351c7509ad38fced37806b2f76fc04a60297ec31ad9f1c67&refreshRate=600000",
+      stream: "traffic-cam/?latlng=39.790728,-104.993914&camId=435226"
   },
   {
-    "id": "camera-24",
-    "name": "I-70 MP 121.8 near Vail Pass",
-    "latitude": 39.6007,
-    "longitude": -106.3685,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-vail/stream.m3u8"
+      id: "camera-24",
+      name: "I-70 MP 244.35 EB at US 6/US 40",
+      latitude: 39.736674,
+      longitude: -105.31732,
+      thumbnail: "https://ie.trafficland.com/v2.0/420742/full?system=weatherbug-web&pubtoken=73a1ce9be1bab9304a4b6395b51a1a24295444e510ad6181d1b869df5134b601&refreshRate=600000",
+      stream: "traffic-cam/?latlng=39.736674,-105.31732&camId=420742"
   },
   {
-    "id": "camera-25",
-    "name": "CO 470 at Santa Fe Dr",
-    "latitude": 39.6212,
-    "longitude": -105.0310,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/co470-santa-fe/stream.m3u8"
+      id: "camera-25",
+      name: "US-6 MP 262.35 EB : 2.2 miles W of Tunnel 3",
+      latitude: 39.735252,
+      longitude: -105.36647,
+      thumbnail: "https://ie.trafficland.com/v2.0/435246/full?system=weatherbug-web&pubtoken=a93dc6e6589a6ee53d2b8bce79237eb99763299bce8dc3ac3ef0875da6b8fe46&refreshRate=600000",
+      stream: "traffic-cam/?latlng=39.735252,-105.36647&camId=435246"
   },
-  {
-    "id": "camera-26",
-    "name": "I-25 MP 100.5 at Fort Collins",
-    "latitude": 40.5853,
-    "longitude": -105.0845,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-fortcollins/stream.m3u8"
-  },
-  {
-    "id": "camera-27",
-    "name": "US 287 at Larimer County Rd 30",
-    "latitude": 40.2041,
-    "longitude": -105.1852,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us287-larimer/stream.m3u8"
-  },
-  {
-    "id": "camera-28",
-    "name": "C-470 at Quebec St",
-    "latitude": 39.6055,
-    "longitude": -104.8987,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/c470-quebec/stream.m3u8"
-  },
-  {
-    "id": "camera-29",
-    "name": "I-25 at Exit 201",
-    "latitude": 39.6341,
-    "longitude": -104.9999,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-exit201/stream.m3u8"
-  },
-  {
-    "id": "camera-30",
-    "name": "US 6 at Clear Creek",
-    "latitude": 39.7573,
-    "longitude": -105.2902,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us6-clearcreek/stream.m3u8"
-  },
-  {
-    "id": "camera-31",
-    "name": "I-25 at Exit 207 towards Park Meadows",
-    "latitude": 39.6020,
-    "longitude": -104.8935,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-parkmeadows/stream.m3u8"
-  },
-  {
-    "id": "camera-32",
-    "name": "US 85 at Trinidad",
-    "latitude": 37.1192,
-    "longitude": -104.5016,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us85-trinidad/stream.m3u8"
-  },
-  {
-    "id": "camera-33",
-    "name": "I-70 MP 275.9 at Idaho Springs",
-    "latitude": 39.7397,
-    "longitude": -105.5144,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-idahosprings/stream.m3u8"
-  },
-  {
-    "id": "camera-34",
-    "name": "I-76 at MP 10.5 near Fort Morgan",
-    "latitude": 40.2522,
-    "longitude": -103.8016,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i76-fortmorgan/stream.m3u8"
-  },
-  {
-    "id": "camera-35",
-    "name": "I-70 MP 212.2 at Mount Vernon",
-    "latitude": 39.7361,
-    "longitude": -105.4167,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-mountvernon/stream.m3u8"
-  },
-  {
-    "id": "camera-36",
-    "name": "I-25 MP 121.5 at Longmont",
-    "latitude": 40.2033,
-    "longitude": -105.0685,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-longmont/stream.m3u8"
-  },
-  {
-    "id": "camera-37",
-    "name": "I-70 MP 193.9 at Silverthorne",
-    "latitude": 39.6545,
-    "longitude": -106.0590,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-silverthorne/stream.m3u8"
-  },
-  {
-    "id": "camera-38",
-    "name": "US 287 at Loveland",
-    "latitude": 40.3964,
-    "longitude": -105.1015,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us287-loveland/stream.m3u8"
-  },
-  {
-    "id": "camera-39",
-    "name": "I-25 at Berthoud",
-    "latitude": 40.2973,
-    "longitude": -105.2340,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-berthoud/stream.m3u8"
-  },
-  {
-    "id": "camera-40",
-    "name": "US 34 at Estes Park",
-    "latitude": 40.3783,
-    "longitude": -105.5200,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us34-estes/stream.m3u8"
-  },
-  {
-    "id": "camera-41",
-    "name": "US 285 at Conifer",
-    "latitude": 39.5703,
-    "longitude": -105.2650,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us285-conifer/stream.m3u8"
-  },
-  {
-    "id": "camera-42",
-    "name": "I-270 MP 4.5 at Pecos St",
-    "latitude": 39.7866,
-    "longitude": -104.8954,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i270-pecos/stream.m3u8"
-  },
-  {
-    "id": "camera-43",
-    "name": "US 50 at Cañon City",
-    "latitude": 38.4402,
-    "longitude": -105.2292,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us50-canon/stream.m3u8"
-  },
-  {
-    "id": "camera-44",
-    "name": "I-70 MP 244.0 at Evergreen",
-    "latitude": 39.6505,
-    "longitude": -105.4002,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-evergreen/stream.m3u8"
-  },
-  {
-    "id": "camera-45",
-    "name": "I-25 at Exit 190 in Pueblo",
-    "latitude": 38.2805,
-    "longitude": -104.6093,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-pueblo/stream.m3u8"
-  },
-  {
-    "id": "camera-46",
-    "name": "US 385 at Burlington",
-    "latitude": 40.1833,
-    "longitude": -102.2637,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us385-burlington/stream.m3u8"
-  },
-  {
-    "id": "camera-47",
-    "name": "I-25 MP 40.0 at Castle Rock",
-    "latitude": 39.3663,
-    "longitude": -104.8936,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i25-castlerock/stream.m3u8"
-  },
-  {
-    "id": "camera-48",
-    "name": "I-70 at MP 204 near Denver",
-    "latitude": 39.7525,
-    "longitude": -105.2267,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-denver/stream.m3u8"
-  },
-  {
-    "id": "camera-49",
-    "name": "US 141 at Dove Creek",
-    "latitude": 37.7449,
-    "longitude": -108.8934,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/us141-dovecreek/stream.m3u8"
-  },
-  {
-    "id": "camera-50",
-    "name": "I-70 MP 225.6 at Lookout Mountain",
-    "latitude": 39.7369,
-    "longitude": -105.2734,
-    "thumbnail": "",
-    "stream": "https://publicstreamer.cotrip.org/rtplive/i70-lookout/stream.m3u8"
-  }
 ];
