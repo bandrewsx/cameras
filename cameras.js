3f4743ec93c6e50b7870a6c113a68f8816f95ca7
Brianna Andrews <bwoandrews@gmail.com>
2024-11-23 09:21:05 -0700

    Add files via upload

---
+++ cameras.js
@@ -0,0 +1,54 @@
+const cameras = [
+  {
+    "id": "camera-1",
+    "name": "28th St and Colorado Ave",
+    "latitude": 40.007581,
+    "longitude": -105.265942,
+    "thumbnail": "https://webappsprod.bouldercolorado.gov/webcams/28th-colorado.jpg",
+    "stream": "https://videostream.bouldercolorado.gov/live/smil:28th_and_colorado.smil/playlist.m3u8"
+  },
+  {
+    "id": "camera-2",
+    "name": "Broadway and Canyon Blvd",
+    "latitude": 40.015830,
+    "longitude": -105.279270,
+    "thumbnail": "https://webappsprod.bouldercolorado.gov/webcams/broadway-canyon.jpg",
+    "stream": "https://videostream.bouldercolorado.gov/live/smil:broadway_and_canyon.smil/playlist.m3u8"
+  },
+  {
+    "id": "camera-3",
+    "name": "Foothills Pkwy and Arapahoe Ave",
+    "location": "Boulder, CO",
+    "thumbnail": "https://webappsprod.bouldercolorado.gov/webcams/foothills-arapahoe.jpg",
+    "stream": "https://videostream.bouldercolorado.gov/live/smil:foothills_and_arapahoe.smil/playlist.m3u8"
+  },
+  {
+    "id": "camera-4",
+    "name": "28th And Iris Ave",
+    "location": "Boulder, CO",
+    "thumbnail": "https://webappsprod.bouldercolorado.gov/webcams/28th-iris.jpg",
+    "stream": "https://videostream.bouldercolorado.gov/live/smil:28th_and_iris.smil/playlist.m3u8"
+  },
+  {
+    "id": "camera-5",
+    "name": "US 36 MP 039.20 EB at CO 157/Foothills Pkwy",
+    "location": "Boulder, CO",
+    "thumbnail": "",
+    "stream": "https://publicstreamer2.cotrip.org/rtplive/036E03920CAM1RP1/chunklist_w744211294.m3u8"
+  },
+  {
+    "id": "camera-6",
+    "name": "US 36 MP 043.15 WB at McCaslin Blvd and US 36 WB On-ramp",
+    "location": "Boulder, CO",
+    "thumbnail": "",
+    "stream": "https://publicstreamer2.cotrip.org/rtplive/036W04315CAM1RP1/chunklist_w1570063898.m3u8"
+  },
+  {
+    "id": "camera-7",
+    "name": "US 36 MP 054.65 WB : 0.3 miles W of Federal Blvd",
+    "location": "Boulder, CO",
+    "thumbnail": "",
+    "stream": "https://publicstreamer3.cotrip.org/rtplive/036W05465CAM1MED/chunklist_w671228802.m3u8"
+  },
+
+];
